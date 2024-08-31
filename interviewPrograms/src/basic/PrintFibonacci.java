13c0c0f2a205b7d506c2df31918a3f8bdb47ec6e
akshaychakale <akshaychakale@gmail.com>
2024-09-01 00:17:15 +0530

    minor chnages

--- interviewPrograms/src/basic/PrintFibonacci.java
+++ interviewPrograms/src/basic/PrintFibonacci.java
@@ -1,2 +1,25 @@
-package basic;public class PrintFibonacci {
+package basic;
+
+import java.util.Scanner;
+
+public class PrintFibonacci {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter count: ");
+        fibonacci(scan.nextInt());
+    }
+
+    public static void fibonacci(int count) {
+        int a = 0, b = 1, c = 1;
+
+        for (int i = 0; i < count; i++) {
+            System.out.print(a + " ");
+
+            a = b;
+            b = c;
+            c = a + b;
+        }
+
+
+    }
 }
