29cc5ac48ac6ff83059b171a3f98f96159077ca8
akshaychakale <akshaychakale@gmail.com>
2024-09-01 00:26:06 +0530

    minor chnages

--- interviewPrograms/src/basic/FindFirstRepeatingCharacter.java
+++ interviewPrograms/src/basic/FindFirstRepeatingCharacter.java
@@ -1,2 +1,27 @@
-package basic;public class FindFirstRepeatingCharacter {
+package basic;
+
+import java.util.Scanner;
+
+public class FindFirstRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        System.out.println(firstCharacter(scan.nextLine()));
+    }
+
+    private static char firstCharacter(String str){
+        char[] ch=str.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = i+1; j < ch.length; j++) {
+                if (ch[i] == ch[j]) {
+                    return ch[i];
+                }
+            }
+        }
+        return '\0';
+    }
+
 }
--- interviewPrograms/src/basic/RemoveDuplicate.java
+++ interviewPrograms/src/basic/RemoveDuplicate.java
@@ -1,2 +1,23 @@
-package basic;public class RemoveDuplicate {
+package basic;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,44,5,6,7,1,3,5,2,6,4,44};
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i=0;i< arr.length;i++){
+            set.add(arr[i]);
+        }
+
+        Iterator it = set.iterator();
+
+        while (it.hasNext()){
+            System.out.print(it.next());
+        }
+    }
 }
