48243298e2de61acd2a4b55cef56f852f2c5410a
akshaychakale <akshaychakale@gmail.com>
2024-09-12 23:59:49 +0530

    added sum of digits

---
+++ interviewPrograms/src/basic/SumOfDigits.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num=12345678;
+
+        int sum=0;
+        while (num!=0){
+            sum+=num%10;
+            num/=10;
+        }
+        System.out.println(sum);
+    }
+}
