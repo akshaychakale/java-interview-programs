e95502f6a6faf18c50c0786ef4699dd3008cfd3c
akshay <akshaychakale@gmail.com>
2024-09-18 22:49:21 +0530

    added decimal to binary conversion code

---
+++ interviewPrograms/src/basic/ConvertDecimalToBinary.java
@@ -0,0 +1,9 @@
+public class ConvertDecimalToBinary {
+	public static void main(String[] args) {
+	
+		int num=123;
+		
+		String binary = Integer.toBinaryString(num);
+		System.out.println(binary);
+	}
+}
