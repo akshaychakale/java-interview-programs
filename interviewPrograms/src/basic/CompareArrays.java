35958508e8ff28d42b7f71bead19558ab3a95801
akshaychakale <akshaychakale@gmail.com>
2024-10-08 18:45:09 +0530

    updated testcase

--- interviewPrograms/src/basic/CompareArrays.java
+++ interviewPrograms/src/basic/CompareArrays.java
@@ -39,13 +39,15 @@ public class CompareArrays {
     }
 
     public static boolean isArrayEquals2(int[] a1, int[] a2) {
-
-        for (int i = 0; i < a1.length; i++) {
-            if (a1[i] != a2[i]) {
-                return false;
+        if (a1.length != a2.length) {
+            for (int i = 0; i < a1.length; i++) {
+                if (a1[i] != a2[i]) {
+                    return false;
+                }
             }
-        }
 
-        return true;
+            return true;
+        }
+        return false;
     }
 }
