29f332b34875a7001b96f562216fe60ed105df0d
akshaychakale <akshaychakale@gmail.com>
2024-10-22 14:57:31 +0530

    added strams examples

---
+++ interviewPrograms/src/practice/StreamsEx1.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamsEx1 {
+    public static void main(String[] args) {
+//        create streams from various sources:
+
+//        1. From Collections
+        List<Integer> myList = Arrays.asList(1, 2, 3);
+        Stream<Integer> stream = myList.stream(); // Sequential stream
+        Stream<Integer> parallelStream = myList.parallelStream(); // Parallel stream
+
+//       2. From Arrays
+        Stream<Integer> arrayStream = Arrays.stream(new Integer[]{1, 2, 3});
+
+//        Using static method
+        Stream<String> stringStream = Stream.of("A", "B", "C");
+
+//        Generating infinite stream
+        Stream<Double> randomNumbers = Stream.generate(Math::random);
+        Stream<Integer> iteratedNumbers = Stream.iterate(0, n -> n + 1);
+    }
+}
