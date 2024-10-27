af03721c8b1f4fea8768348513714a8bc59ab026
akshaychakale <akshaychakale@gmail.com>
2024-10-27 20:22:51 +0530

    updated practice code

--- interviewPrograms/src/practice/functionalProgramming/FP02Exercise.java
+++ interviewPrograms/src/practice/functionalProgramming/FP02Exercise.java
@@ -1,5 +1,7 @@
 package practice.functionalProgramming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,12 +15,23 @@ public class FP02Exercise {
        int result2 = printSumOfOddNumbersInList(List.of(1, 23, 56, 454, 65, 11, 22, 54));
         System.out.println(result2);
 
-        //Create a list of even numbers from a list
-        List result3 = printEvenNumbersFromList(List.of(1, 23, 56, 454, 65, 11, 22, 54));
+        //Create a list with even numbers filtered from a list
+        List<Integer> result3 = printEvenNumbersFromList(List.of(1, 23, 56, 454, 65, 11, 22, 54));
         System.out.println(result3);
+
+        //create a list with length of course titles
+        List<String> result4 = printCourseWithLengthOfTitles(List.of("Spring", "Spring boot", "microservices", "api", "kotlin", "android"));
+        System.out.println(result4);
+
     }
 
-    private static List printEvenNumbersFromList(List<Integer> numbers) {
+    private static List<String> printCourseWithLengthOfTitles(List<String> courses) {
+        return courses.stream()
+                .map(course ->course+" "+course.length())
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> printEvenNumbersFromList(List<Integer> numbers) {
        return numbers.stream()
                 .filter(num -> num % 2 == 0)
                 .collect(Collectors.toList());
