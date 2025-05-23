772cc4dc5a80a2db71fc3fafd75c0d4e5a025615
akshaychakale <akshaychakale@gmail.com>
2025-05-23 11:57:22 +0530

    updated

--- interviewPrograms/src/basic/FindDuplicate.java
+++ interviewPrograms/src/basic/FindDuplicate.java
@@ -1,15 +1,30 @@
 package basic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindDuplicate {
     public static void main(String[] args) {
 
         String[] cars = {"Honda", "KIA", "tata", "KIA"};
 
-        for (int i = 0; i < cars.length; i++) {
-            for (int j = i + 1; j < cars.length; j++) {
-                if (cars[i].equals(cars[j])) {
-                    System.out.println("Duplicate element : " + cars[i]);
-                }
+//        for (int i = 0; i < cars.length; i++) {
+//            for (int j = i + 1; j < cars.length; j++) {
+//                if (cars[i].equals(cars[j])) {
+//                    System.out.println("Duplicate element : " + cars[i]);
+//                }
+//            }
+//        }
+        findDuplicate(cars);
+    }
+
+    private static void findDuplicate(String[] strArray){
+        Set<String> set = new HashSet<>();
+
+        System.out.println("Duplicates: ");
+        for (String s: strArray){
+            if(!set.add(s)){
+                System.out.println(s);
             }
         }
     }
--- interviewPrograms/src/basic/FindMissingNumber.java
+++ interviewPrograms/src/basic/FindMissingNumber.java
@@ -1,8 +1,11 @@
 package basic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindMissingNumber {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 7, 8};
+        int[] nums = {1, 2, 3, 4, 5, 7, 8,10};
         int length = nums.length + 1;
 
         int expected_sum = length * (length + 1) / 2;
@@ -16,5 +19,25 @@ public class FindMissingNumber {
         System.out.println("expected_sum " + expected_sum);
         System.out.println("actualSum " + actualSum);
         System.out.println("missing number is : " + (expected_sum - actualSum));
+
+        findMissingNumbers(nums);
+    }
+
+    public static void findMissingNumbers(int[] arr){
+        Set<Integer> set = new HashSet<>();
+
+        for (int s: arr){
+            set.add(s);
+        }
+
+        int low=arr[0];
+        int max=arr[arr.length-1];
+
+        System.out.println("missing numbers: ");
+        for (int i = low; i < max; i++) {
+            if (!set.contains(i)){
+                System.out.print(i+" ");
+            }
+        }
     }
 }
